3a3eb103ce5dc485cc570864f0876701b55e38b0
yoorimseo <seou1225@naver.com>
2022-10-08 16:38:51 +0900

    (10/08) 문제1 ~ 문제10

---
+++ 010.js
@@ -0,0 +1,40 @@
+// * 문제10 : 별 찍기
+
+// * 입력 : 5
+// * 출력 :
+//     *
+//    ***
+//   *****
+//  *******
+// *********
+
+// ! 내 풀이
+// const num = prompt("숫자를 입력하세요 : ");
+const num = 5;
+let gap = num;
+let star = 1;
+
+for (let i = 0; i < num; i++) {
+    gap--;
+    console.log(" ".repeat(gap) + "*".repeat(star));
+    star += 2;
+}
+
+// ! 정답
+// const num = prompt("숫자를 입력하세요 : ");
+// const n = 5;
+// let tree = "";
+
+// for (let i = 1; i <= n; i++) {
+//     let star = "";
+//     for (let j = 1; j <= n - i; j++) {
+//         star += " ";
+//     }
+//     for (let k = 1; k <= 2 * i - 1; k++) {
+//         star += "*";
+//     }
+
+//     tree += star + "\n";
+// }
+
+// console.log(tree);
--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # js-100
-CODE FESTIVAL JavaScript 100제
+
+## CODE FESTIVAL JavaScript 100제
+
+JavaScript로 푸는 100개의 코딩 문제
