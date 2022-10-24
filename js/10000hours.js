c7d9dc65b203946ceb00667136eebd46b431a7f9
yoorimseo <seou1225@naver.com>
2022-10-25 07:22:45 +0900

    [feat] 사용자 입력값을 바탕으로 결과를 보여주는 기능 추가

--- js/10000hours.js
+++ js/10000hours.js
@@ -17,3 +17,24 @@ btnShowRes.addEventListener("click", () => {
         // console.log("입력 안함!!");
     }
 });
+
+const txtResExpert = document.querySelector(".res-expert");
+const txtResTraningTime = document.querySelector(".res-training-time");
+
+// * 사용자의 입력값을 바탕으로 결과를 보여준다
+function showRes() {
+    // * '나는 며칠 동안 훈련을 해야 1만 시간이 될까?' 버튼을 클릭하면 하단에 결과가 담긴 section이 보여지게 한다
+    sectRes.style.display = "block";
+
+    // * 사용자가 어떤 전문가가 되기로 했는지 텍스트로 보여준다
+    txtResExpert.textContent = `${inpExpert.value}`;
+
+    // * 해당 전문가가 되기 위해 며칠 이상 훈련해야 하는지 계산해서 텍스트로 보여준다
+    // * 10000만 시간 = x일 * 하루에 훈련하는 시간
+    // * x일 = Math.floor(10000 / 하루에 훈련하는 시간)
+    const resDay = Math.floor(10000 / inpTraingTime.value);
+    txtResTraningTime.textContent = `${resDay}`;
+}
+
+// * '훈련하러 가기 GO! GO!' 버튼을 클릭하면 모달창이 뜬다.
+// * '종료하고 진짜 훈련하러 가기 GO! GO!' 버튼을 클릭하면 모달창을 닫는다.
