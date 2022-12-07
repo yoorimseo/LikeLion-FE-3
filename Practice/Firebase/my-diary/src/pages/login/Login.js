f8d3baba98a2a4a8fe2775bb66b308866139832c
yoorimseo <seou1225@naver.com>
2022-12-07 17:22:46 +0900

    (12월 7일) 재현강사님 실습

--- Practice/Firebase/my-diary/src/pages/login/Login.js
+++ Practice/Firebase/my-diary/src/pages/login/Login.js
@@ -1,38 +1,63 @@
-import { useState } from 'react'
-import styles from './Login.module.css'
+import { useState } from 'react';
+import { useLogin } from '../../hooks/useLogin';
+import styles from './Login.module.css';
 
 export default function Login() {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const { error, isPending, login } = useLogin();
 
-    const [email, setEmail] = useState('');
-    const [password, setPassword] = useState('');
-
-    const handleData = (event) => {
-        console.log(event.target.type);
-        if (event.target.type === 'email') {
-            setEmail(event.target.value);
-        } else if (event.target.type === 'password') {
-            setPassword(event.target.value);
-        }
+  const handleData = (event) => {
+    console.log(event.target.type);
+    if (event.target.type === 'email') {
+      setEmail(event.target.value);
+    } else if (event.target.type === 'password') {
+      setPassword(event.target.value);
     }
+  };
 
-    const handleSubmit = (event) => {
-        event.preventDefault();
-        console.log(email, password);
-    }
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    login(email, password);
+  };
 
-    return (
-        <form className={styles.login_form} onSubmit={handleSubmit}>
-            <fieldset>
-                <legend>로그인</legend>
+  return (
+    <form
+      className={styles.login_form}
+      onSubmit={handleSubmit}
+    >
+      <fieldset>
+        <legend>로그인</legend>
 
-                <label htmlFor="myEmail">email : </label>
-                <input type="email" id='myEmail' required value={email} onChange={handleData} />
+        <label htmlFor='myEmail'>email : </label>
+        <input
+          type='email'
+          id='myEmail'
+          required
+          value={email}
+          onChange={handleData}
+        />
 
-                <label htmlFor="myPassWord">password : </label>
-                <input type="password" id='myPassWord' required value={password} onChange={handleData} />
+        <label htmlFor='myPassWord'>password : </label>
+        <input
+          type='password'
+          id='myPassWord'
+          required
+          value={password}
+          onChange={handleData}
+        />
 
-                <button type='submit' className={styles.btn}>로그인</button>
-            </fieldset>
-        </form>
-    )
+        {!isPending && (
+          <button
+            type='submit'
+            className={styles.btn}
+          >
+            로그인
+          </button>
+        )}
+        {isPending && <strong>로그인중입니다...</strong>}
+        {error && <strong>{error}</strong>}
+      </fieldset>
+    </form>
+  );
 }
